ba4ebfa914e382392c6c3052d4da00b4bf0919a3
Andrei Zhuk1 <Andrei_Zhuk1@epam.com>
2018-11-22 15:21:02 +0300

    dasd

--- app/__init__.py
+++ app/__init__.py
@@ -7,26 +7,47 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 
 def webhook():
-    print("webhook"); sys.stdout.flush()
+    sys.stdout.flush()
     if request.method == 'POST' and request.headers['content-type'] == 'application/x-www-form-urlencoded':
         data = json.loads(request.form['payload'])
         print(data)
-        return 'dddddd', 200
+        dispatcher.bot.send_message(chat_id=chats[0], text=getTravisMessage(data))
+        return '', 200
     elif request.method == 'POST' and request.headers['content-type'] == 'application/json':
         data = json.loads(request.data)
-        print(request.json)
-        return 'dddddd', 200
+        print(data)
+        message = getTravisMessage(data)
+        sendToAllChats(message)
+        return '', 200
     else:
         abort(400)
 
+def getTravisMessage(data):
+    repo = data['repository']['name']
+    type = data['type']
+    state = data['state']
+    branch = data['config']['deploy'][0]['on']['branch'][0]
+    link = data['build_url']
+    return 'Travis CI \n\n' + 'Repository: ' + repo + '\n' + 'The build of type ' + type + ' of ' + branch + ' branch ' + ' has changed state to ' + state + '\n' + 'Link: ' + link
+
+def sendToAllChats(message):
+    for chat in chats:
+        dispatcher.bot.send_message(chat_id=chat, text=message)
+
+
+
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import apiai, json
+
+chats = []
 
 
 updater = Updater(token='709802155:AAGXZEVvNoT6FQL7JuU2SBw9L8j53UlGGG4')
 dispatcher = updater.dispatcher
 
 def startCommand(bot, update):
+    if not update.message.chat_id in chats:
+        chats.append(update.message.chat_id)
     bot.send_message(chat_id=update.message.chat_id, text='привет, давай пообщаемся?')
 def textMessage(bot, update):
     request = apiai.ApiAI('05fbecb26f1349e0892e2f61f5970a52').text_request()
@@ -48,3 +69,9 @@ dispatcher.add_handler(start_command_handler)
 dispatcher.add_handler(text_message_handler)
 
 updater.start_polling(clean=True)
+
+if __name__ == '__main__':
+    app.run()
+
+updater.idle()
+
