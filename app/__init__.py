320f3ed5456e451895878db309eae3f1f4e4d59b
Andrei-Zhuk <zhuk_andrey@tut.by>
2018-11-22 11:51:52 +0300

    adjust s

--- app/__init__.py
+++ app/__init__.py
@@ -19,14 +19,13 @@ def webhook():
     else:
         abort(400)
 
-# Настройки
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import apiai, json
 
 
-updater = Updater(token='709802155:AAGXZEVvNoT6FQL7JuU2SBw9L8j53UlGGG4') # Токен API к Telegram
+updater = Updater(token='709802155:AAGXZEVvNoT6FQL7JuU2SBw9L8j53UlGGG4')
 dispatcher = updater.dispatcher
-# Обработка команд
+
 def startCommand(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся?')
 def textMessage(bot, update):
@@ -41,11 +40,11 @@ def textMessage(bot, update):
         bot.send_message(chat_id=update.message.chat_id, text=response)
     else:
         bot.send_message(chat_id=update.message.chat_id, text='Я Вас не совсем понял!')
-# Хендлеры
+
 start_command_handler = CommandHandler('start', startCommand)
 text_message_handler = MessageHandler(Filters.text, textMessage)
-# Добавляем хендлеры в диспетчер
+
 dispatcher.add_handler(start_command_handler)
 dispatcher.add_handler(text_message_handler)
-# Начинаем поиск обновлений
+
 updater.start_polling(clean=True)
